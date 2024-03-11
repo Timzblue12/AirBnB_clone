13a2a06c94b4da2374d69677a75b321863384e34
Timothy Ofie <timzblue@gmail.com>
2024-03-11 07:31:09 -0700

    updated tests

--- models/place.py
+++ models/place.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""
-Module for the Place class.
-"""
+"""Defines the Place class."""
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
     """Represent a place.
-
     Attributes:
         city_id (str): The City id.
         user_id (str): The User id.
